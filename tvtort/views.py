3ee415b26a08f3e2f35cb11835fb67eac1fb7292
agr2003 <agr2003@gmail.com>
2013-05-15 16:48:23 +0400

    Views

--- tvtort/views.py
+++ tvtort/views.py
@@ -1,18 +1,31 @@
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.template import loader, Context
 from django.http import HttpResponse
+from tvtort.models import SeriesCountry
+
 
 def archive(request):
     t = loader.get_template("base.html")
     return HttpResponse(t)
 
+
 def index(request):
-    return HttpResponse("HelloWorld!")
+    latest_countries = SeriesCountry.objects.order_by()
+    template = loader.get_template("tvtort/index.html")
+    context = Context({"latest_countries": latest_countries})
+    return HttpResponse(template._render(context))
+
 
 def detail(request, num):
-    return HttpResponse("You're looking at tvtort %s." % num)
+    country = get_object_or_404(SeriesCountry, pk=num)
+    return render(request, 'tvtort/detail.html', {"country": country})
+
 
 def results(request, num):
     return HttpResponse("You're looking at the results of tvtort %s." % num)
 
+
 def vote(request, num):
     return HttpResponse("You're voting on tvtort %s." % num)
+
+
